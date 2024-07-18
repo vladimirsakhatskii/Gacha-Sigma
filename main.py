6d6c5f8b67e7aa945ff765d724dd938532736ed1
Admin <s_vladimir902@mail.ru>
2024-07-18 21:56:47 +0600

    .

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-bot = Bot(os.getenv['TOKEN'])
-dp = Dispatcher(bot=bot, storage=storage)
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    print('Bot started!')
+async def send_welcome(message: types.Message):
+    await bot.send_message(
+        text=f'{message.from_user.first_name}, добро пожаловать!',
+        chat_id=message.from_user.id)
+    await message.answer(text='Желаю тебе удачи на просторах этой гачи)')
+
+@dp.message_handler(commands=['cod'])
+async def cod(message: types.Message):
+    await message.answer(text='Код')
+
+@dp.message_handler(commands=['play'])
+async def play(message: types.Message):
+    await message.answer(text = 'Игра')
     
 if __name__ == '__main__':
+    print('Bot started!')
     executor.start_polling(dp)
