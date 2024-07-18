fcb067c3dc87090a3a2d291cda2818ebe8dae4c5
Admin <s_vladimir902@mail.ru>
2024-07-18 21:06:02 +0600

    .

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
 
-bot = Bot(token="TOKEN")
+load_dotenv()
+bot = Bot(os.getenv['TOKEN'])
+dp = Dispatcher(bot=bot, storage=storage)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    print('Bot started!')
+    
+if __name__ == '__main__':
+    executor.start_polling(dp)
